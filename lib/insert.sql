0529975b4a83448f7f25fa8fe67780f519da90d1
Malcome Fein <mfein90@gmail.com>
2022-12-25 10:03:18 -0500

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 6, "M", "black brown", "choleric", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 9, "M", "white", "melancholic", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 7, "F", "brown", "phlegmatic", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 3, "M", "white", "sanguine", 1);
+INSERT INTO bears VALUES (5,"Melissa", 4, "F", "brown", "choleric", 1);
+INSERT INTO bears VALUES (6,"Grinch", 5, "M", "Gray", "melancholic", 0);
+INSERT INTO bears VALUES (7,"Wendy", 9, "F", "white", "phlegmatic", 0);
+INSERT INTO bears VALUES (8,null, 8, "F", "black", "sanguine", 0);
